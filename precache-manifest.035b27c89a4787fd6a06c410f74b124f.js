2ed1d8945a6ef429b9c93af2231f607b0a65ef0a
victor Farias <victorfarias.tv@gmail.com>
2021-09-22 13:25:44 -0300

    Updates

---
+++ precache-manifest.035b27c89a4787fd6a06c410f74b124f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837f409fde355da135c8c60137849042",
+    "url": "/movie-cards-library-crud/index.html"
+  },
+  {
+    "revision": "397a52161b2262e749f7",
+    "url": "/movie-cards-library-crud/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "7fa53215e81359fe58ae",
+    "url": "/movie-cards-library-crud/static/js/2.4492ec1d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie-cards-library-crud/static/js/2.4492ec1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "397a52161b2262e749f7",
+    "url": "/movie-cards-library-crud/static/js/main.6f843157.chunk.js"
+  },
+  {
+    "revision": "bb30b542c60389f24c86",
+    "url": "/movie-cards-library-crud/static/js/runtime-main.8c4eb3d2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f567dabbbc958dd4530341355bc22b2.js"
+  "/movie-cards-library-crud/precache-manifest.035b27c89a4787fd6a06c410f74b124f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie-cards-library-crud/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
